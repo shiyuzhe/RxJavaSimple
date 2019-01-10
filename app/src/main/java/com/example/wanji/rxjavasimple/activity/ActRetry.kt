eb3af919ae0deba21377abbd3bc9618d73539b58
shiyuzhe <18091664559@163.com>
2019-01-10 17:43:23 +0800

    syz:retry,dispose when double call retry()

--- app/src/main/java/com/example/wanji/rxjavasimple/activity/ActRetry.kt
+++ app/src/main/java/com/example/wanji/rxjavasimple/activity/ActRetry.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.wanji.rxjavasimple.R
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.act_retry.*
@@ -21,29 +22,33 @@ class ActRetry : AppCompatActivity() {
         }
     }
 
+    private var disposable: Disposable? = null
     /**
      * 重连三次
      * 测试：第三次重连时模拟请求成功，前两次发送错误
      */
     fun retry() {
         var num = 0//记录重连次数
-        Observable.timer(1, TimeUnit.SECONDS).doOnSubscribe {
-            System.out.println("subscribing")
-        }.map {
-            if (++num > 2)
-                return@map 1
-            throw  RuntimeException()
-        }.retryWhen {
-            val counter = AtomicInteger()
-            it.takeWhile {
-                counter.getAndIncrement() != 3
-            }.flatMap {
-                System.out.println("delay retry by " + counter.get() + " second(s)")
-                Observable.timer(counter.toLong(), TimeUnit.SECONDS)
-            }
-        }.observeOn(AndroidSchedulers.mainThread())
+        disposable = Observable.timer(1, TimeUnit.SECONDS)
+                .map {
+                    if (++num > 2)
+                        return@map 1
+                    throw  RuntimeException()
+                }.retryWhen {
+                    val counter = AtomicInteger()
+                    it.takeWhile {
+                        counter.getAndIncrement() != 3
+                    }.flatMap {
+                        System.out.println("delay retry by " + counter.get() + " second(s)")
+                        Observable.timer(counter.toLong(), TimeUnit.SECONDS)
+                    }
+                }.observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
-                .subscribeBy {
+                .doOnSubscribe {
+                    //retryWhen之后调用一次，之前每次重连都调用
+                    disposable?.dispose()
+                    System.out.println("subscribing")
+                }.subscribeBy {
                     System.out.println("subscribeBy$it")
                 }
     }
